4e3261170802d5826fad2cc47097e25aa52d9a0b
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-06-25 19:05:43 +0300

    popup is toggling

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let editButton = document.querySelector('.button_type_edit');
+let closeButton = document.querySelector('.button_type_close');
 let popup = document.querySelector('.popup');
 
-function open() {
-  popup.classList.add('popup_opened');
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
 }
 
-editButton.addEventListener('click', open);
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
