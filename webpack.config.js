81042b17a710b396a531663cc42f40c6ee849a3d
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-09-05 11:02:02 +0300

    renamed script folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/scripts/index.js' },
+  entry: { main: './src/components/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
